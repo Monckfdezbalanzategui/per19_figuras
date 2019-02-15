1232d0276a6d358bd3ee38b6478dddb10638bd8b
Monckfdezbalanzategui <46898680+Monckfdezbalanzategui@users.noreply.github.com>
2019-02-15 16:57:02 +0100

    Add files via upload

---
+++ figuras.py
@@ -0,0 +1,16 @@
+class Shape:
+    def __init__(self):
+
+    def calculateArea():
+        return
+    def calculatePerimeter():
+        return
+class Square(Shape):
+    def __init__(self,side_length):
+        self.side_length=side_length
+    def calculateArea(self):
+        resultado=side_length**2
+        return resultado
+    def calculatePerimeter(self):
+        resultado=side_length*4
+        return resultado
